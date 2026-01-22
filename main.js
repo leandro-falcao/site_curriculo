e2223022532932dba1ba392fbb23096084d622e0
leandro-falcao <leandrofalcaoti@gmail.com>
2026-01-22 13:29:22 -0300

    Update main.js
    update

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+//inserir tag link de css importante
+let headInserirLink = document.querySelector("head").insertAdjacentHTML('beforeend', `<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/leandro-falcao/site_curriculo/x-style.css">`);
+// inserido
 
+// pesquisar a tag link dentro de head(todas as tags)
+let heads = document.querySelectorAll("head > link")
+// contador de itens buscados de head> filho <link>
+let qtdLinks = heads.length;
+
+let penultimoTagLink = heads[(qtdLinks-2)]
